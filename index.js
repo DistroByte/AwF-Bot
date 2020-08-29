02f5f467401bce432b6af6045c18adafc4614cf4
oof2win2 <honza.kocourek.parklane@gmail.com>
2020-08-29 16:24:36 +0200

    Added slap feature for Jammy

--- index.js
+++ index.js
@@ -69,6 +69,11 @@ client.on('message', (message) => {
   }
   if (message.author.bot) return;
   if (message.content.includes('lenny')) message.channel.send(`( ͡° ͜ʖ ͡°)`);
+  if (message.content.includes('Jammy slap') && message.content.includes('<@')) {
+    message.mentions.users.forEach(user => {
+      message.content = message.content.replace(/<@[\S.]*>/, '@'+user.username+' :clap:');
+    });
+  }
 
   //checking for mentions and replacing the user/channel id with the name
   if (message.content.includes('<@')) { //check if the message that the bot reads has a mention of a user
