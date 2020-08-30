2f0bd8be80ba785b4ed0e447b5b4f488d38de8e0
oof2win2 <honza.kocourek.parklane@gmail.com>
2020-08-30 18:56:24 +0200

    Added spaces to JLOGGER messages

--- control.lua
+++ control.lua
@@ -4,13 +4,13 @@ handler.add_lib(require("silo-script"))
 
 -- Grafana logging
 local function log1(event) --player death
-	print ("JLOGGER: DIED: " .. game.get_player(event.player_index).name .. event.cause.name or "no_cause") --jammylogger
+	print ("JLOGGER: DIED: " .. game.get_player(event.player_index).name .. ' ' .. event.cause.name or "no_cause") --jammylogger
 end
 local function log2()  --rocket launched
 	print ("JLOGGER: ROCKET: " .. "ROCKET_LAUNCHED") --jammylogger
 end
 local function log3(event)  --handcraft
-	print ("JLOGGER: HANDCRAFT: " .. event.item_stack.name or "unnamed_item" .. game.get_player(event.player_index)) --jammylogger
+	print ("JLOGGER: HANDCRAFT: " .. event.item_stack.name or "unnamed_item" .. ' ' .. game.get_player(event.player_index)) --jammylogger
 end
 local function log4(event)  --capsule
 	print ("JLOGGER: CAPSULE:" .. game.get_player(event.player_index).name or "no_player" .. event.item.name) --jammylogger
@@ -19,7 +19,7 @@ local function log5(event)  --research finished
 	print ("JLOGGER: RESEARCH FINISHED: " .. event.research.name) --jammylogger
 end
 local function log6(event)  --fired artillery
-	print ("JLOGGER: ARTILLERY: " .. event.entity.name .. event.source.name or "no_source") --jammylogger
+	print ("JLOGGER: ARTILLERY: " .. event.entity.name .. ' ' .. event.source.name or "no_source") --jammylogger
 end
 
 
