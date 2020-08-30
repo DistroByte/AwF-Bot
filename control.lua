3ee4263a9ee0c278988b6d2a72f01ff07d8d1b5b
oof2win2 <honza.kocourek.parklane@gmail.com>
2020-08-30 19:08:37 +0200

    Made control.lua more human-readable

--- control.lua
+++ control.lua
@@ -3,30 +3,33 @@ handler.add_lib(require("freeplay"))
 handler.add_lib(require("silo-script"))
 
 -- Grafana logging
-local function log1(event) --player death
+local function logDeath(event) --player death
 	print ("JLOGGER: DIED: " .. game.get_player(event.player_index).name .. ' ' .. event.cause.name or "no_cause") --jammylogger
 end
-local function log2()  --rocket launched
+local function logRocket()  --rocket launched
 	print ("JLOGGER: ROCKET: " .. "ROCKET_LAUNCHED") --jammylogger
 end
-local function log3(event)  --handcraft
+local function logHandcraft(event)  --handcraft
 	print ("JLOGGER: HANDCRAFT: " .. event.item_stack.name or "unnamed_item" .. ' ' .. game.get_player(event.player_index)) --jammylogger
 end
-local function log4(event)  --capsule
-	print ("JLOGGER: CAPSULE:" .. game.get_player(event.player_index).name or "no_player" .. event.item.name) --jammylogger
+local function logCapsule(event)  --capsule
+	print ("JLOGGER: CAPSULE:" .. event.item.name .. game.get_player(event.player_index).name or "no_player") --jammylogger
 end
-local function log5(event)  --research finished
-	print ("JLOGGER: RESEARCH FINISHED: " .. event.research.name) --jammylogger
+local function logResearch(event)  --research finished
+	print ("JLOGGER: RESEARCH FINISHED: " .. event.research.name .. ' ' event.research.) --jammylogger
 end
-local function log6(event)  --fired artillery
+local function logArtillery(event)  --fired artillery
 	print ("JLOGGER: ARTILLERY: " .. event.entity.name .. ' ' .. event.source.name or "no_source") --jammylogger
 end
 
 
 -- Data collection
-script.on_event(defines.events.on_player_died,              function(event) log1(event) end)
-script.on_event(defines.events.on_rocket_launched,          function(event) log2() end)
-script.on_event(defines.events.on_player_crafted_item,      function(event) log3(event) end)
-script.on_event(defines.events.on_player_used_capsule,      function(event) log4(event) end)
-script.on_event(defines.events.on_research_finished,        function(event) log5(event) end)
-script.on_event(defines.events.on_trigger_fired_artillery,  function(event) log6(event) end)
+script.on_event(defines.events.on_player_died,              function(event) logDeath(event) end)
+script.on_event(defines.events.on_rocket_launched,          function(event) logRocket() end)
+script.on_event(defines.events.on_player_crafted_item,      function(event) logHandcraft(event) end)
+script.on_event(defines.events.on_player_used_capsule,      function(event) logCapsule(event) end)
+script.on_event(defines.events.on_research_finished,        function(event) logResearch(event) end)
+script.on_event(defines.events.on_trigger_fired_artillery,  function(event) logArtillery(event) end)
+
+-- Jammy feedback to ? commands on Discord
+script.on_event(defines.events.on_player_banned, )
