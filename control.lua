196ef7bceefe276ba3557db548c0f812d60f269b
oof2win2 <honza.kocourek.parklane@gmail.com>
2020-08-30 13:58:20 +0200

    Updated control.lua - added some logging
    Added some logging to the control.lua, should log most things we care about (production etc)

--- control.lua
+++ control.lua
@@ -9,8 +9,12 @@ local function log(msg)
 end
 
 -- Data collection
-script.on_event(defines.events.on_player_died,            function(event) log(event, "died", cause) end)
+script.on_event(defines.events.on_player_died,            function(event, player_index, cause) log(event, "died", cause) end)
 script.on_event(defines.events.on_rocket_launched,        function(event) log(event, "rocket launched") end)
+script.on_event(defines.events.on_player_crafted_item,    function(event, item_stack, player_index) log(event, item_stack, "handcrafted by", player_index) end)
+script.on_event(defines.events.on_research_finished, function(event, research) log(event, "research", research) end)
+script.on_event(defines.events.on_player_used_capsule, function(event, player_index, item) log(event, "capsule", player_index, item) end)
+script.on_event(defines.events.on_trigger_fired_artillery, function(event, entity, source) log(event, "artillery fired", entity, source) end)
 
 
 
