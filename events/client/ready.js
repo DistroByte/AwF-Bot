bbc57b5355e61509bbd904e702afd79811052b58
James Hackett <jamesthackett1@gmail.com>
2020-08-31 23:08:02 +0000

    :sparkles: Feat:: adds a ready event

---
+++ events/client/ready.js
@@ -0,0 +1,20 @@
+module.exports = (client) => {
+  console.log(
+    `${client.user.username} is online: ${new Date().toString().slice(4, 24)}`
+  );
+
+  let activities = [
+    `${client.guilds.cache.size} servers!`,
+    `${client.channels.cache.size} channels!`,
+    `${client.users.cache.size} users!`,
+  ],
+    i = 0;
+  setInterval(
+    () =>
+      client.user.setActivity(
+        `${client.prefix}help | ${activities[i++ % activities.length]}`,
+        { type: 'WATCHING' }
+      ),
+    15000
+  );
+};
