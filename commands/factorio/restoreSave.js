7a9d11322fbd85790309772f883c54bfced34d6b
oof2win2 <honza.kocourek.parklane@gmail.com>
2020-10-12 21:21:15 +0200

    restoreSave.js works, tested. Has the ability to restore saves

--- commands/factorio/restoreSave.js
+++ commands/factorio/restoreSave.js
@@ -1,7 +1,8 @@
 const { sendToAll } = require('../../functions')
-const { fs } = require('fs')
+const fs = require('fs')
 const { exec } = require("child_process")
 const { absPath } = require('../../botconfig.json');
+const Discord = require('discord.js')
 
 module.exports = {
   config: {
@@ -14,77 +15,59 @@ module.exports = {
   },
   run: async (client, message, args) => {
     let authRoles = message.member.roles.cache
-
     if (authRoles.some(r => r.name === 'Admin') || authRoles.some(r => r.name === 'Moderator') || authRoles.some(r => r.name === 'dev')) {
       if (!args[0]) { // no argument at all
         let choiceEmbed = new Discord.MessageEmbed()
-          .setTitle('Server Rollback Choices');
-          .setDescription('Choices of a Factorio Server Rollback. This shows **all** folders in the `servers` directory, some may not be Factorio server folders');
-          .setColor('GREEN');
-          .setAuthor(`${message.guild.me.displayName} Help`, message.guild.iconURL);
-          .setThumbnail(client.user.displayAvatarURL());
+          .setTitle('Server Rollback Choices')
+          .setDescription('Choices of a Factorio Server Rollback. This shows **all** folders in the `servers` directory, some may not be Factorio server folders')
+          .setColor('GREEN')
+          .setAuthor(`${message.guild.me.displayName} Help`, message.guild.iconURL)
+          .setThumbnail(client.user.displayAvatarURL())
           .setFooter(
             `© ${message.guild.me.displayName} | Developed by DistroByte & oof2win2 | Total Commands: ${client.commands.size}`,
             client.user.displayAvatarURL()
-          );
-        await fs.readdir('../servers/', (err, files) => { // add in all file names that end with .zip
-          if (err) console.log(err);
-          else {
-            files.forEach(file => {
-              if (file.endsWith('.zip')) choiceEmbed.addField(file, '\u200B');
-            });
-          }
-        })
+          )
+        let dirData = fs.readdirSync('../servers/')
+        dirData.forEach(dir => {
+          if (fs.statSync('../servers/'+dir).isDirectory()) choiceEmbed.addField(`\`${dir}\``, '\u200B'); //check if it is a directory and if yes add it to the embed
+        });
         return message.channel.send(choiceEmbed)
       }
       if (!args[1]) { // no second argument, only server name
         let choiceEmbed = new Discord.MessageEmbed()
-          .setTitle('Server Rollback Choices');
-          .setDescription('Choices of a Factorio Server Rollback. This shows **all** .zip files, some may not be Factorio saves');
-          .setColor('GREEN');
-          .setAuthor(`${message.guild.me.displayName} Help`, message.guild.iconURL);
-          .setThumbnail(client.user.displayAvatarURL());
+          .setTitle('Server Rollback Choices')
+          .setDescription('Choices of a Factorio Server Rollback. This shows **all** .zip files, some may not be Factorio saves')
+          .setColor('GREEN')
+          .setAuthor(`${message.guild.me.displayName} Help`, message.guild.iconURL)
+          .setThumbnail(client.user.displayAvatarURL())
           .setFooter(
             `© ${message.guild.me.displayName} | Developed by DistroByte & oof2win2 | Total Commands: ${client.commands.size}`,
             client.user.displayAvatarURL()
-          );
-          await fs.readdir('../servers/'+args[0], (err, files) => { // add in all file names that end with .zip
-            if (err) console.log(err);
-            else {
-              files = files.map(function (fileName) {
-                return {
-                  name: fileName,
-                  time: fs.statSync(dir + '/' + fileName).mtime.getTime()
-                };
-              })
-              .sort(function (a, b) {
-                return a.time - b.time; })
-              .map(function (v) {
-                return v.name; });
-              for (let i = 0; i < 25; i++) { // max number of fields in a Discord Embed is 25
-                if (!files[i]) break;
-                // console.log(files[i])
-                if (files[i]){
-                  choiceEmbed.fields.push({name: files[i], value: 'hello'});
-                  console.log(files[i], '1')
-                }
-              }
-            }
-          })
+          )
+        let dir = '../servers/'+args[0]
+        let dirData = fs.readdirSync(dir) // add in all file names that end with .zip
+        for (let i = 0; i < 25; i++) { // max number of fields in a Discord Embed is 25
+          if (!dirData[i]) break;
+          if (dirData[i] && dirData[i].endsWith('.zip')) {
+            let data = fs.statSync(dir+'/'+dirData[i])
+            let date = new Date(data.birthtimeMs)
+            choiceEmbed.addField(`\`${dirData[i]}\``, `Save created on: ${date.toUTCString()}`);
+          }
+        }
         return message.channel.send(choiceEmbed)
       }
       if (args[0] && args[1]) { // both server name and save name are provided
         let choiceEmbed = new Discord.MessageEmbed()
-          .setTitle('Server Rollback');
-          .setDescription('Choices of a Factorio Server Rollback. This shows **all** .zip files, some may not be Factorio saves');
-          .setColor('GREEN');
-          .setAuthor(`${message.guild.me.displayName} Help`, message.guild.iconURL);
-          .setThumbnail(client.user.displayAvatarURL());
+          .setTitle('Server Rollback')
+          .setDescription('Choices of a Factorio Server Rollback. This shows **all** .zip files, some may not be Factorio saves')
+          .setColor('GREEN')
+          .setAuthor(`${message.guild.me.displayName} Help`, message.guild.iconURL)
+          .setThumbnail(client.user.displayAvatarURL())
           .setFooter(
             `© ${message.guild.me.displayName} | Developed by DistroByte & oof2win2 | Total Commands: ${client.commands.size}`,
             client.user.displayAvatarURL()
-          );
-          .addField(`Rolling back server ${args[0]} to save ${args[1]}`);
+          )
+          .addField(`Rolling back server ${args[0]} to save ${args[1]}`, '\u200B');
         exec(absPath+'/'+args[0]+'/factorio-init/factorio stop', (error, stdout, stderr) => { //stop the factorio server
           if (error) {
               console.log(`server stop error: ${error.message}`);
