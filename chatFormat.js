84001a1f82d6a1e2b3da4837e46b0cb1908bbbf0
oof2win2 <honza.kocourek.parklane@gmail.com>
2020-11-08 17:51:41 +0000

    factorio ban etc. feedback added

--- chatFormat.js
+++ chatFormat.js
@@ -2,7 +2,52 @@ const functions = require('./functions');
 const { filterBan } = require("./filterBan");
 
 module.exports = function chatFormat(line, channel, client) {
-  const helpdesk = client.channels.cache.get('590241134740111387')
+  const helpdesk = client.channels.cache.get('590241134740111387');
+  const moderators = client.channels.cache.get('697146357819113553');
+
+  if (line.includes('[KICK] ')) {
+    line = line.slice(line.indexOf('[KICK] ') + '[KICK] '.length);
+    line = line.split(' ');
+    const player = line[0];
+    const doneBy = line[4];
+    const reason = line.slice(6)
+    console.log(reason);
+    moderators.send(`Player \`${player}\` has been KICKED by \`${doneBy}\` for reason \`${reason}\``);
+    return client.channels.cache.get(channel).send(`Player \`${player}\` has been KICKED by \`${doneBy}\` for reason \`${reason}\``);
+  }
+  if (line.includes('[BAN] ')) {
+    line = line.slice(line.indexOf('[BAN] ') + '[BAN] '.length);
+    line = line.split(' ');
+    const player = line[0];
+    const doneBy = line[4];
+    const reason = line.slice(6)
+    moderators.send(`Player \`${player}\` has been BANNED by \`${doneBy}\` for reason \`${reason}\``);
+    return client.channels.cache.get(channel).send(`Player \`${player}\` has been BANNED by \`${doneBy}\` for reason \`${reason}\``);
+  }
+  if (line.includes('[MUTED] ')) {
+    line = line.slice(line.indexOf('[MUTED] ') + '[MUTED] '.length);
+    line = line.split(' ');
+    const player = line[0];
+    const doneBy = line[4];
+    moderators.send(`Player \`${player}\` has been MUTED by \`${doneBy}\``);
+    return client.channels.cache.get(channel).send(`Player \`${player}\` has been MUTED by \`${doneBy}\``);
+  }
+  if (line.includes('[UNBANNED] ')) {
+    line = line.slice(line.indexOf('[UNBANNED] ') + '[UNBANNED] '.length);
+    line = line.split(' ');
+    const player = line[0];
+    const doneBy = line[4];
+    moderators.send(`Player \`${player}\` has been UNBANNED by \`${doneBy}\``);
+    return client.channels.cache.get(channel).send(`Player \`${player}\` has been UNBANNED by \`${doneBy}\``);
+  }
+  if (line.includes('[UNMUTED] ')) {
+    line = line.slice(line.indexOf('[UNMUTED] ') + '[UNMUTED] '.length);
+    line = line.split(' ');
+    const player = line[0];
+    const doneBy = line[4];
+    moderators.send(`Player \`${player}\` has been UNMUTED by \`${doneBy}\``);
+    return client.channels.cache.get(channel).send(`Player \`${player}\` has been UNMUTED by \`${doneBy}\``);
+  }
 
   if (line.includes('?griefer')) {
     //mentions 548545406653431810 (Admin) and 555824650324672522 (Moderator)
