0fd717e624c1302e60c08129105bd8082fe5c87e
oof2win2 <honza.koco44@gmail.com>
2021-05-03 17:01:29 +0200

    :bug: Bugfix: Documentation with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+'use strict'
+module.exports = function (grunt) {
+    grunt.initConfig({
+        jsdoc2md: {
+            // oneOutputFile: {
+            //     src: '/*.js',
+            //     dest: 'docs/documentation.md'
+            // },
+            separateOutputFilePerInput: {
+                files: [
+                    { src: 'helpers/fifo-handler.js', dest: 'docs/md/fifo-handler.md' },
+                    { src: 'helpers/rcon.js', dest: 'docs/md/rcon.md' },
+                    { src: 'helpers/serverHandler.js', dest: 'docs/md/serverHandler.md' },
+                    { src: 'helpers/serverUPSHandler.js', dest: 'docs/md/serverUPSHandler.md' },
+                ]
+            },
+            // withOptions: {
+            //     options: {
+            //         'no-gfm': false
+            //     },
+            //     src: 'src/wardrobe.js',
+            //     dest: 'api/with-index.md'
+            // }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-jsdoc-to-markdown')
+    grunt.registerTask('default', 'jsdoc2md')
+}
