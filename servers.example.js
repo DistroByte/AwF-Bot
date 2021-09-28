9c35fd018ce00f569f8f2aabfedccdd55a2a1969
oof2win2 <honza.koco44@gmail.com>
2021-09-28 19:06:31 +0000

    :bug: Fix example servers file

--- servers.example.js
+++ servers.example.js
@@ -1,20 +1,21 @@
 module.exports = [
   {
-    name: "DEV-DUMP",   // server name in logs
-    discordname: "dev-dump",  // discord channel name for the server
+    name: "DEV-DUMP", // server name in logs
+    discordname: "dev-dump", // discord channel name for the server
     discordid: "723280139982471247", // id of the discord channel where server messages will be sent
     path: "test", // where the server is located relative to "serverpath" in ./config.js
     rconoffset: 0, // rcon port offset from "rconport" in ./config.js
-    
+
     // whether https://mods.factorio.com/mod/awf-graftorioMod
     // or an alternative with the same formatis installed
     enabledGraftorio: true,
-    toWatch: { // things to watch in the server's directory
-      serverOut: true,  // vanilla, chat log
+    toWatch: {
+      // things to watch in the server's directory
+      serverOut: true, // vanilla, chat log
       awfLogging: true, // requires just the mod
       datastore: false, // requires the full scenario, not just the mod
-      discord: false    // requires the full scenario, not just the mod
+      discord: false, // requires the full scenario, not just the mod
     },
     dev: false, // whether or not the server is developmental
   },
-}
+];
